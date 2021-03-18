1159619191f4e7958ea77c9da20056de122b5dcf
Renan <rnnsf@yahoo.com>
2021-03-18 17:19:42 -0300

    Criado atributo de classe olhos.

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,4 +1,6 @@
 class Pessoa: #criação de objetos
+    olhos = 2 #como olhos sempre serão 2, não é necessário colocar no def init
+    #cria o atributo defaut (atributo de classe). o valor é o mesmo para todos os objetos
     def __init__(self, *filhos, nome = None, idade = 25): #criando atributos de dados
         self.idade = idade
         self.nome = nome #campo existe mas sem valor atribuido
@@ -20,5 +22,13 @@ if __name__ == '__main__':
         print(filho.nome)
     silva.sobrenome = 'Faria'
     del silva.filhos
+    silva.olhos = 1 #ele passa a fazer parte do __dict__ do objeto silva, alterado de 2 para 1
+    del silva.olhos #apaga o atributo do objeto e não o da classe, então ele vai de 1 para 3
     print(silva.__dict__)
     print(renan.__dict__)
+    Pessoa.olhos = 3 #isso fará q a classe pessoa passe de 2 para 3. mas o silva continua com 1
+    #print(Pessoa.nome) #não faz sentido fazer isso pq não especificou qual pessoa
+    print(Pessoa.olhos) #isso faz sentido, pq toda pessoa tem 2 olhos. atributo de classe pode ser acessado pela classe
+    print(silva.olhos)
+    print(renan.olhos) #é possivel acessar o atributo de classe não só pela classe mas tbm pelos objetos
+    print(id(Pessoa.olhos), id(renan.olhos), id(silva.olhos))
