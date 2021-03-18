a1cd9953eb2d50d34b3c01a8b395ec5746505c0e
Renan <rnnsf@yahoo.com>
2021-03-18 16:41:35 -0300

    Criado e removido atributo dinamico de objeto do tipo Pessoa.

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -18,3 +18,7 @@ if __name__ == '__main__':
     print(silva.idade)
     for filho in silva.filhos:
         print(filho.nome)
+    silva.sobrenome = 'Faria'
+    del silva.filhos
+    print(silva.__dict__)
+    print(renan.__dict__)
